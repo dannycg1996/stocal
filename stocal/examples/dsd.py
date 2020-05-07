bc3d5ab909c375668bd27d969d18f78382b570e3
danny <dannycg1996@gmail.com>
2020-05-07 16:45:04 +0100

    Rewritten the Binding function to be much more 'flexible', using regex to open and close brackets. However, re.fullmatch() does not work as I thought it did, so error checking needs to be updated so as to make sure every <, { and [ has a corresponding >, }, ].

--- stocal/examples/dsd.py
+++ stocal/examples/dsd.py
@@ -33,42 +33,47 @@ import re
 alpha = 1.e-10
 beta = 1000**-2
 
-initial_state = {"{S' N^* L' R'}": 60, "<L N^ M N^>": 50, "<N^* L' R'>": 60, "{L N^ M N^ R}": 50}
+#initial_state = {"{S' N^* L' R'}": 60, "<L N^ M N^>": 50}
+#initial_state = {"{ N^* L' R'}": 60, "<L N^ M N^>": 50}
+initial_state = {"{N^* S' N^*}[A B C^]": 60, "<N^ M N^>": 50}
 format_check = re.compile('<[^\[\]{}]*?>|\[[^<>{}]*?]|{[^<>\[\]]*?}|\s*:*')
 
+bound_th = re.compile('(\w)(?=\^)(?=[^\[\]]*])')
 upper_th = re.compile('(\w)(?=\^\s)(?=[^<>]*>)|(\w)(?=\^>)(?=[^<>]*>)')
-upper_th_c = re.compile('(\w)(?=\^\*)(?=[^<>]*>)')
-lower_th = re.compile('(\w)(?=\^\s)(?=[^{}]*})|(\w)(?=\^})(?=[^{}]*})')
 lower_th_c = re.compile('(\w)(?=\^\*)(?=[^{}]*})')
+start = re.compile('[\[{]*?(<)|[\[<]*?({)|[<{]*?(])')
+end = re.compile('[\]}]*?(>)|[\]>]*?(})|[>}]*?(])')
 empty_bracket = re.compile('(<(?:\s)*>)|({(?:\s)*})')
 
 class BindingRule(stocal.TransitionRule):
     """Join any two strings into their concatenations"""
     Transition = stocal.MassAction
 
+    #TODO: Stop this binding rule from allowing any binding on to toeholds on overhangs.
+    #Either don't match on them (how?) or regex check for toeholds on overhangs in if __name__ == '__main__':
+
     def novel_reactions(self, k, l):
-        print(k)
-        print(l)
+        print("k:", k)
+        print("l:", l)
         yield from self.toehold_binding(k, l, upper_th, lower_th_c)
-        yield from self.toehold_binding(k, l, lower_th, upper_th_c)
-        yield from self.toehold_binding(k, l, upper_th_c, lower_th)
         yield from self.toehold_binding(k, l, lower_th_c, upper_th)
 
     def toehold_binding(self, k, l, regex_1, regex_2):
         for matching_1 in re.finditer(regex_1, k):
             for matching_2 in re.finditer(regex_2, l):
                 if matching_1.group() == matching_2.group():
-                    if regex_1 == upper_th or regex_1 == upper_th_c:
+                    if regex_1 == upper_th:
                         print("upper_th")
-                        part_A = k[:matching_1.start()] + ">" + l[:matching_2.start()] + "}"
-                        part_B ="{" + l[matching_2.end()+2:] + "<" + k[matching_1.end()+2:]
-                    elif regex_1 == lower_th or regex_1 == lower_th_c:
-                        print("lower")
-                        part_A = k[:matching_1.start()] + "}" + l[:matching_2.start()] + ">"
-                        part_B = "<" + l[matching_2.end()+2:] + "{" + k[matching_1.end()+2:]
+                        part_A = k[:matching_1.start()] + re.search(end,k[matching_1.start():]).group() + l[:matching_2.start()] + re.search(end,l[matching_2.start():]).group()
+                        part_B = re.search(start, l[:matching_2.end()]).group() + l[matching_2.end()+2:] + re.search(start, k[:matching_1.end()+1]).group() + k[matching_1.end()+1:]
+                    else:
+                        print("lower_th")
+                        part_A = k[:matching_1.start()] + re.search(end,k[matching_1.start():]).group() + l[:matching_2.start()] + re.search(end,l[matching_2.start():]).group()
+                        part_B = re.search(start, l[:matching_2.end()]).group() + l[matching_2.end()+1:] + re.search(start, k[:matching_1.end()+1]).group() + k[matching_1.end()+2:]
+
                     draft_strand = part_A + "[" + matching_2.group() + "^]" + part_B
                     final_strand = re.sub(empty_bracket, '', draft_strand)
-                    print(final_strand)
+                    print("final", final_strand)
                     yield self.Transition([k, l], [final_strand], alpha)
 
 process = stocal.Process(
@@ -77,12 +82,37 @@ process = stocal.Process(
 
 if __name__ == '__main__':
     format_correct = True
-    for key in initial_state:
-        if re.fullmatch(format_check, key) is None:
-            format_correct = False
-            print("Incorrect Input") #Some of the brackets must not match up. Raise a proper error in time.
+  #TODO:  re.fullmatch() does not work as I thought it did, so error checking needs to be updated so as to make sure every <, { and [ has a corresponding >, }, ].
+    #for key in initial_state:
 
-    if format_correct:
-        traj = process.sample(initial_state, tmax=100.)
-        for _ in traj:
-            print(traj.time, traj.state)
+        # if re.fullmatch(format_check, key) is None:
+        #     format_correct = False
+        #     print("Incorrect Input:", key) #Some of the brackets must not match up. Raise a proper error in time.
+
+    #if format_correct:
+    traj = process.sample(initial_state, tmax=100.)
+    for _ in traj:
+        print(traj.time, traj.state)
+
+#Extension (Allow toehold complements on the upper strand and toeholds on the lower strand)
+#upper_th_c = re.compile('(\w)(?=\^\*)(?=[^<>]*>)')
+#lower_th = re.compile('(\w)(?=\^\s)(?=[^{}]*})|(\w)(?=\^})(?=[^{}]*})')
+#         yield from self.toehold_binding(k, l, lower_th, upper_th_c)
+#         yield from self.toehold_binding(k, l, upper_th_c, lower_th)
+
+    # def toehold_binding(self, k, l, regex_1, regex_2):
+    #     for matching_1 in re.finditer(regex_1, k):
+    #         for matching_2 in re.finditer(regex_2, l):
+    #             if matching_1.group() == matching_2.group():
+    #                 if regex_1 == upper_th or regex_1 == upper_th_c:
+    #                     print("upper_th")
+    #                     part_A = k[:matching_1.start()] + ">" + l[:matching_2.start()] + "}"
+    #                     part_B ="{" + l[matching_2.end()+2:] + "<" + k[matching_1.end()+2:]
+    #                 elif regex_1 == lower_th or regex_1 == lower_th_c:
+    #                     print("lower")
+    #                     part_A = k[:matching_1.start()] + "}" + l[:matching_2.start()] + ">"
+    #                     part_B = "<" + l[matching_2.end()+2:] + "{" + k[matching_1.end()+2:]
+    #                 draft_strand = part_A + "[" + matching_2.group() + "^]" + part_B
+    #                 final_strand = re.sub(empty_bracket, '', draft_strand)
+    #                 print(final_strand)
+    #                 yield self.Transition([k, l], [final_strand], alpha)
