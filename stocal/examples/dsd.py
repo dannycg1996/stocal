2918a5c623b7b78f1f043f71522ead40e4bab145
danny <dannycg1996@gmail.com>
2020-05-08 16:30:38 +0100

    Updated DSD Code. Added a new class for Unbinding Rule and deleted some unused functions and condensed some regex, to improve structure. Both rules need to be extensively tested for cases with overhang rules.
    Unbinding rule works reasonably well, although the joining of the 'parts' to the prefix and suffix needs further thought.
    Added comments.
    Unbind rule could still do with some refactoring, although the first goal is to get it working properly.

--- stocal/examples/dsd.py
+++ stocal/examples/dsd.py
@@ -37,27 +37,23 @@ beta = 1000**-2
 #initial_state = {"{ N^* L' R'}": 60, "<L N^ M N^>": 50}
 #initial_state = {"{N^* S' N^*}[C^]": 60, "<N^ M N^>": 50, "{L'}<L>[N^]<R>[M^]{P'}<S'>[A^]{B}" : 50}
 initial_state = {"{L'}<L>[N^]<R>{R'}" : 50}
-format_check = re.compile('<[^\[\]{}]*?>|\[[^<>{}]*?]|{[^<>\[\]]*?}|\s*:*')
 
-#bound_th = re.compile('(\w)(?=\^)(?=[^\[\]]*])')
-#double_toehold = re.compile('\[\W*?(\w)\^\W*?\]')
-double_toehold = re.compile('(?:\[\W*?(\w)(?:\^\W*?\]))')
-double_th_label = re.compile('(\w)(?=\^)(?=[^<>{}]*])')
-upper_th = re.compile('(\w)(?=\^\s)(?=[^<>]*>)|(\w)(?=\^>)(?=[^<>]*>)')
-lower_th_c = re.compile('(\w)(?=\^\*)(?=[^{}]*})')
-start = re.compile('[\[{]*?(<)|[\[<]*?({)|[<{]*?(])')
-end = re.compile('[\]}]*?(>)|[\]>]*?(})|[>}]*?(])')
-empty_bracket = re.compile('(<(?:\s)*>)|({(?:\s)*})')
+format_check = re.compile('<[^\[\]{}]*?>|\[[^<>{}]*?]|{[^<>\[\]]*?}|\s*:*') #TODO: Fix this - speak to Harold.
+# Needs to check that each part of a strand meets one of these requirements. Fullmatch does not work.
 
-#last_upper_th = re.compile('<([^>]*)>*(?!(?:.*<|.*\[))')
-last_upper_th = re.compile('<([^>]*)>')
-last_lower_th = re.compile('{([^}]*)}')
-#last_lower_th = re.compile('({[^}]*)}*(?!(?:.*{|.*\[))')
-next_upper_th = re.compile('<([^>]*)>*')
-next_lower_th = re.compile('{([^}]*)}*')
+double_toehold = re.compile('(?:\[\W*?(\w)(?:\^\W*?\]))') #Matches on double toeholds of the form [A^] not [A^ B]
+double_th_label = re.compile('(\w)(?=\^)(?=[^<>{}]*])') #Returns the label of a double toehold regex. Better way of doing this?
+upper_th_label = re.compile('(\w)(?=\^\s)(?=[^<>]*>)|(\w)(?=\^>)(?=[^<>]*>)') #Returns the labels of upper toeholds.
+lower_th_c_label = re.compile('(\w)(?=\^\*)(?=[^{}]*})') #Returns labels of lower toeholds/
+next_open_bracket = re.compile('[\[{]*?(<)|[\[<]*?({)|[<{]*?(])') #Matches on open brackets [, { and <
+next_close_bracket = re.compile('[\]}]*?(>)|[\]>]*?(})|[>}]*?(])') #Matches on close brackets ], } and >
+empty_bracket = re.compile('(<(?:\s)*>)|({(?:\s)*})') #Matches on empty brackets like <>, {} and [ ].
 
-open_spaces = re.compile('<(\s)+?\w|{(\s)+?\w|\[(\s)+?\w')
-close_spaces = re.compile('(?:\w|\^|\*)(\s)+?>|(?:\w|\^|\*)(\s)+?\]|(?:\w|\^|\*)(\s)+?}')
+upper_sequence = re.compile('((?<=\<).+?(?=\>))') #Pulls through characters between < and > chars, excluding the brackets themselves.
+lower_sequence = re.compile('((?<=\{).+?(?=\}))') #Pulls through characters between { and } chars, excluding the brackets themselves.
+
+spaces_at_start = re.compile('<(\s)+?\w|{(\s)+?\w|\[(\s)+?\w') #Matches on spaces between brackets and words, like < A or { B
+spaces_at_end = re.compile('(?:\w|\^|\*)(\s)+?>|(?:\w|\^|\*)(\s)+?\]|(?:\w|\^|\*)(\s)+?}') #Matches on spaces before bracket closes.
 
 class BindingRule(stocal.TransitionRule):
     """Join any two strings into their concatenations"""
@@ -67,63 +63,67 @@ class BindingRule(stocal.TransitionRule):
     #Either don't match on them (how?) or regex check for toeholds on overhangs in if __name__ == '__main__':
 
     def novel_reactions(self, k, l):
-        #print("k:", k)
-        #print("l:", l)
-        #yield from self.toehold_binding(k, l, upper_th, lower_th_c)
-        #yield from self.toehold_binding(k, l, lower_th_c, upper_th)
-        yield from self.toehold_unbinding(k)
-        yield from self.toehold_unbinding(l)
+        yield from self.toehold_binding(k, l, upper_th_label, lower_th_c_label)
+        yield from self.toehold_binding(k, l, lower_th_c_label, upper_th_label)
 
     def toehold_binding(self, k, l, regex_1, regex_2):
         for matching_1 in re.finditer(regex_1, k):
             for matching_2 in re.finditer(regex_2, l):
                 if matching_1.group() == matching_2.group():
-                    if regex_1 == upper_th:
+                    if regex_1 == upper_th_label:
                         print("upper_th")
-                        part_A = k[:matching_1.start()] + re.search(end,k[matching_1.start():]).group() + l[:matching_2.start()] + re.search(end,l[matching_2.start():]).group()
-                        part_B = re.search(start, l[:matching_2.end()]).group() + l[matching_2.end()+2:] + re.search(start, k[:matching_1.end()+1]).group() + k[matching_1.end()+1:]
+                        part_A = k[:matching_1.start()] + re.search(next_close_bracket, k[matching_1.start():]).group() + l[:matching_2.start()] + re.search(next_close_bracket, l[matching_2.start():]).group()
+                        part_B = re.search(next_open_bracket, l[:matching_2.end()]).group() + l[matching_2.end() + 2:] + re.search(next_open_bracket, k[:matching_1.end() + 1]).group() + k[matching_1.end() + 1:]
                     else:
                         print("lower_th")
-                        part_A = k[:matching_1.start()] + re.search(end,k[matching_1.start():]).group() + l[:matching_2.start()] + re.search(end,l[matching_2.start():]).group()
-                        part_B = re.search(start, l[:matching_2.end()]).group() + l[matching_2.end()+1:] + re.search(start, k[:matching_1.end()+1]).group() + k[matching_1.end()+2:]
+                        part_A = k[:matching_1.start()] + re.search(next_close_bracket, k[matching_1.start():]).group() + l[:matching_2.start()] + re.search(next_close_bracket, l[matching_2.start():]).group()
+                        part_B = re.search(next_open_bracket, l[:matching_2.end()]).group() + l[matching_2.end() + 1:] + re.search(next_open_bracket, k[:matching_1.end() + 1]).group() + k[matching_1.end() + 2:]
 
                     draft_strand = part_A + "[" + matching_2.group() + "^]" + part_B
                     final_strand = re.sub(empty_bracket, '', draft_strand)
                     print("final", final_strand)
                     yield self.Transition([k, l], [final_strand], alpha)
 
-    def toehold_unbinding(self, k):
-        k = re.sub(open_spaces, '', k)
-        k = re.sub(close_spaces, '', k)
-        print('k:', k)
-        for double_th in re.finditer(double_toehold, k):
-            print(k[:double_th.start()])
+class UnbindingRule(stocal.TransitionRule):
+    """Splits two strings when a toehold unbinds"""
+    Transition = stocal.MassAction
+
+    #TODO: Does this rule still work when toeholds are involved?
+    def novel_reactions(self, kl):
+        yield from self.toehold_unbinding(kl)
+
+    def toehold_unbinding(self, kl):
+        kl = re.sub(spaces_at_start, '', kl)
+        kl = re.sub(spaces_at_end, '', kl)
+        print('kl:', kl)
+        for double_th in re.finditer(double_toehold, kl):
+            print(kl[:double_th.start()])
             label = re.search(double_th_label, double_th.group()).group()
             prefix, suffix, upper_1, lower_1, upper_2, lower_2 = "", "", "", "", "", ""
-            bracket_open = k[:double_th.start()].rfind(']')
-            bracket_close = k[double_th.end():].find('[')
+            bracket_open = kl[:double_th.start()].rfind(']')
+            bracket_close = kl[double_th.end():].find('[')
             if bracket_open !=-1:
-                prefix = k[:bracket_open]
+                prefix = kl[:bracket_open]
             else:
                 bracket_open = 0
 
             if bracket_close != -1:
-                suffix = k[bracket_close:]
+                suffix = kl[bracket_close:]
             else:
                 bracket_close = len(double_th.group())
 
-            if re.search(last_upper_th, k[bracket_open:double_th.start()]) is not None:
-                upper_1 = re.search(last_upper_th, k[bracket_open:double_th.start()]).group()
-            if re.search(last_lower_th, k[bracket_open:double_th.start()]) is not None:
-                lower_1 = re.search(last_lower_th, k[bracket_open:double_th.start()]).group()
-            if re.search(next_upper_th, k[double_th.end()+1:bracket_close]) is not None:
-                upper_2 = re.search(next_upper_th, k[double_th.end()+1:bracket_close]).group()
-            if re.search(last_lower_th, k[double_th.end():bracket_close]) is not None:
-                lower_2 = re.search(next_lower_th, k[double_th.end():bracket_close]).group()
+            if re.search(upper_sequence, kl[bracket_open:double_th.start()]) is not None:
+                upper_1 = re.search(upper_sequence, kl[bracket_open:double_th.start()]).group()
+            if re.search(lower_sequence, kl[bracket_open:double_th.start()]) is not None:
+                lower_1 = re.search(lower_sequence, kl[bracket_open:double_th.start()]).group()
+            if re.search(upper_sequence, kl[double_th.end() + 1:bracket_close]) is not None:
+                upper_2 = re.search(upper_sequence, kl[double_th.end() + 1:bracket_close]).group()
+            if re.search(lower_sequence, kl[double_th.end():bracket_close]) is not None:
+                lower_2 = re.search(lower_sequence, kl[double_th.end():bracket_close]).group()
 
             #TODO: STOP the matchings (upper_th next etc) from including the brackets!!!! HOW?!
             print("prefix" ,prefix)
-            print("search range", re.search(last_upper_th, k[bracket_open:double_th.start()]))
+            print("search range", re.search(upper_sequence, kl[bracket_open:double_th.start()]))
             print(bracket_open)
             print(double_th.start())
             print("upper 1", upper_1)
@@ -132,17 +132,16 @@ class BindingRule(stocal.TransitionRule):
             print("lower 1", lower_1)
             print("lower 2", lower_2)
             print("suffix", suffix)
-            part_A = prefix + "<" + upper_1[] + " " + label + "^ " + upper_2 + ">"
+            part_A = prefix + "<" + upper_1 + " " + label + "^ " + upper_2 + ">"
             part_A_fin = re.sub(empty_bracket, '', part_A)
             part_B = suffix + "{" + lower_1 + " " + label + "^*" + lower_2 + "}"
             part_B_fin = re.sub(empty_bracket, '', part_B)
             print("Final A:    ", part_A_fin)
             print("Final B:    ", part_B_fin)
-            yield self.Transition([k], [part_A_fin, part_B_fin], alpha)
-
+            yield self.Transition([kl], [part_A_fin, part_B_fin], alpha)
 
 process = stocal.Process(
-    rules=[BindingRule()]
+    rules=[BindingRule(), UnbindingRule()]
 )
 
 if __name__ == '__main__':
@@ -159,12 +158,15 @@ if __name__ == '__main__':
     for _ in traj:
         print(traj.time, traj.state)
 
-#Extension (Allow toehold complements on the upper strand and toeholds on the lower strand)
-#upper_th_c = re.compile('(\w)(?=\^\*)(?=[^<>]*>)')
-#lower_th = re.compile('(\w)(?=\^\s)(?=[^{}]*})|(\w)(?=\^})(?=[^{}]*})')
-#         yield from self.toehold_binding(k, l, lower_th, upper_th_c)
-#         yield from self.toehold_binding(k, l, upper_th_c, lower_th)
+#Unused code is below, in case of extensions being needed or previous regex being needed again.
 
+# Extension (Allow toehold complements on the upper strand and toeholds on the lower strand)
+# upper_th_c = re.compile('(\w)(?=\^\*)(?=[^<>]*>)')
+# lower_th = re.compile('(\w)(?=\^\s)(?=[^{}]*})|(\w)(?=\^})(?=[^{}]*})')
+#     yield from self.toehold_binding(k, l, lower_th, upper_th_c)
+#     yield from self.toehold_binding(k, l, upper_th_c, lower_th)
+
+#Original toehold_binding function
     # def toehold_binding(self, k, l, regex_1, regex_2):
     #     for matching_1 in re.finditer(regex_1, k):
     #         for matching_2 in re.finditer(regex_2, l):
@@ -182,14 +184,9 @@ if __name__ == '__main__':
     #                 print(final_strand)
     #                 yield self.Transition([k, l], [final_strand], alpha)
 
-
-     #     print(re.findall(double_toehold_label,matching.group()))
-        #     print("experiment", matching)
-        # if len(re.findall(double_toehold, k)) == 1 and k.count('[') == 1:
-        #     for matching in re.finditer(double_toehold, k):
-        #         print(matching.group())
-        #     print("findall", re.search(double_toehold, k))
-        #     print("findall2", re.search(double_toehold, k).group())
-        #     print(len(re.findall(double_toehold, k)))
-        #    # print("finditer", re.finditer(double_toehold, k).group())
-        #     yield self.Transition([k], [k], alpha)
+#bound_th = re.compile('(\w)(?=\^)(?=[^\[\]]*])')
+#double_toehold = re.compile('\[\W*?(\w)\^\W*?\]')
+#last_upper_th = re.compile('<([^>]*)>*(?!(?:.*<|.*\[))')
+#last_upper_th = re.compile('<([^>]*)>')
+#last_lower_th = re.compile('{([^}]*)}')
+#last_lower_th = re.compile('({[^}]*)}*(?!(?:.*{|.*\[))')
